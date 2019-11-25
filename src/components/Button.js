3f94f6375f504ef5cc00273557d6ed6a5c9cbc7b
Vanessa Kroeker <vanessakr@Vanessas-MacBook-Pro.local>
2019-11-25 12:38:01 -0800

    render button components, refactor with classNames

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
+let classNames = require('classnames');
+
 
 export default function Button(props) {
-   return <></>;
+   
+   const buttonClass = classNames("button",  {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   })
+
+
+   return (
+      <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
+
 }
+
+// function Application(props) {
+//    return (
+//    <main>
+//       <Button onClick={(event) => action("button-clicked")}>Disabled</Button>
+//    </main>
+//    );
+// }
