128b40b89916dae7d6ff3b137824138f836e16aa
vkro <vkroeker@gmail.com>
2019-11-25 15:38:07 -0800

    dayClass applies class based on selected day and num spots available

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./DayListItem.scss";
+let classNames = require("classnames");
+
 
 export default function DayListItem(props) {
+  // applies day-list__item--selected and day-list__item--full styles
+  // based on props passed to DLI using classnames lib
+  const dayClass = classNames({
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  })
+
+
   return (
-    <li onClick{...() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots}</h3>
     </li>
