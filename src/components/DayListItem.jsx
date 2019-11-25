902d563e690f8264e6445e0b8248c1cb13b3e5e8
vkro <vkroeker@gmail.com>
2019-11-25 14:31:14 -0800

    added ... for storybook to run

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DayListItem(props) {
   return (
-    <li onClick{() => props.setDay(props.name)}>
+    <li onClick{...() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots}</h3>
     </li>
