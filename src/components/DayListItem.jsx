0ff31c062ad8a8d9e633a179de3192084fcab886
vkro <vkroeker@gmail.com>
2019-11-25 15:58:23 -0800

    format props.spots to make tests pass

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -11,11 +11,17 @@ export default function DayListItem(props) {
     "day-list__item--full": !props.spots
   })
 
+  const formatSpots = (
+    (props.spots === 0) ? `no spots remaining` : 
+    (props.spots === 1) ? `1 spot remaining` :
+    `${props.spots} spots remaining`
+  );
+
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots}</h3>
     </li>
   );
 };
