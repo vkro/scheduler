f791fac5b397c692acce6452c9b6833c01ea125b
vkro <vkroeker@gmail.com>
2019-11-25 20:29:30 -0800

    add props to items in interviewer list

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
 
 
-export default function DayList(props) {
+export default function InterviewerList(props) {
 
   const mapInterviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
-        key={interviewer.id}
-
+        interviewer={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={props.setInterviewer}
       />
     )
   })
-
-
-  
-
-
 
   return (<section className="interviewers">
           <h4 className="interviewers__header text--light">Interviewer</h4>
