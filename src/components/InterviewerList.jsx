8c00c8c6b49e500f132f87438002ae59003685d7
vkro <vkroeker@gmail.com>
2019-11-25 21:10:19 -0800

    prop thread id prop from interview list item up to interview list

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -8,11 +8,11 @@ export default function InterviewerList(props) {
   const mapInterviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
-        interviewer={interviewer.id}
+        key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
-        setInterviewer={props.setInterviewer}
+        setInterviewer={props.setInterviewer(interviewer.id)}
       />
     )
   })
