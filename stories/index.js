a1768de8b5bb178fbb0e83e1b1752c8ad7ea2981
vkro <vkroeker@gmail.com>
2019-11-26 13:43:21 -0800

    create Status component and import to stories index

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 //Initiates Storybook and registers DayListItem component
 storiesOf("DayListItem", module)
