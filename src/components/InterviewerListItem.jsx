2e97f9f9a8711a8fd44dece75b54003e8a1f72a3
vkro <vkroeker@gmail.com>
2019-11-25 19:29:25 -0800

    interviewer component for Interview List

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+let classNames = require("classnames");
+
+
+export default function InterviewerListItem(props) {
+
+
+  return (
+    <li></li>
+  )
+};
