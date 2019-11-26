f63d24307b3999f735b2b224ca7ffa919830fc2e
vkro <vkroeker@gmail.com>
2019-11-26 12:23:58 -0800

    add Appointment component and return empty html article

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,3 +2,9 @@ import React from "react";
 import "components/Appointment/styles.scss"
 
 
+export default function Appointment(props) {
+  
+  
+  return (<article className="appointment"></article>)
+
+};
