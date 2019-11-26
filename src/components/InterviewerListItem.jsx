1aa8cd016ef1aff27dea394d1250077473db981f
vkro <vkroeker@gmail.com>
2019-11-25 19:51:41 -0800

    add props to interviewer list item

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
-let classNames = require("classnames");
-
+//let classNames = require("classnames");
 
 export default function InterviewerListItem(props) {
 
 
   return (
-    <li className="interviewers__item">
+    <li className="interviewers__item" onClick={() => props.setInterviewer(props.name)}>
     <img
       className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
+      src={props.avatar}
+      alt={props.name}
     />
-    Sylvia Palmer
+      {props.name}
   </li>
   )
 };
 
 
+// id:number - the id of the interviewer
+// name:string - the name of the interviewer
+// avatar:url - a url to an image of the interviewer
+// selected:boolean - to determine if an interview is selected or not
+// setInterviewer:function - sets the interviewer upon selection
