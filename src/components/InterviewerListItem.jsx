a2c36b3e990a185440953bc82f3d10a894d62080
vkro <vkroeker@gmail.com>
2019-11-25 20:00:31 -0800

    delete reference comments

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -19,10 +19,3 @@ export default function InterviewerListItem(props) {
   </li>
   )
 };
-
-
-// id:number - the id of the interviewer
-// name:string - the name of the interviewer
-// avatar:url - a url to an image of the interviewer
-// selected:boolean - to determine if an interview is selected or not
-// setInterviewer:function - sets the interviewer upon selection
