a906ff8cc070214ea9ea3183f715fbce401c61a7
vkro <vkroeker@gmail.com>
2019-11-26 19:18:04 -0800

    add story for booked appointment fragment

--- stories/index.js
+++ stories/index.js
@@ -215,7 +215,17 @@ storiesOf("DayList", module)
       .add("Appointment Empty", () => (
         <Fragment>
           <Appointment id={1} time="12pm" />
-          <Appointment id="last" time="1pm"/>
+          <Appointment id="last" time="1pm" />
+        </Fragment>
+      ))
+      .add("Appointment Booked", () => (
+        <Fragment>
+          <Appointment
+            id={1}
+            time="12pm"
+            interview={{ student: "Lydia Miller-Jones", interviewer }}
+          />
+          <Appointment id="last" time="1pm" />
         </Fragment>
       ))
 
