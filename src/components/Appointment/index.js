d8577d20402ae0283d1b24a3142b152ccb454e1a
vkro <vkroeker@gmail.com>
2019-11-26 19:12:40 -0800

    render Header and either Show or Empty interview spot in Appointment component (index.js)

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "components/Appointment/styles.scss"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 
 export default function Appointment(props) {
   
+  const showInterview = (
+    props.interview ? <Show student={props.name} interviewer={props.interviewer}/> : <Empty onAdd={props.onAdd}/>
+  )
   
-  return (<article className="appointment"></article>)
+  return (<article className="appointment">
+    <Header
+    time={props.time}
+    />
+    {showInterview}
+  </article>)
 
 };
