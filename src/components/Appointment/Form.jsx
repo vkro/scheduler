6c8c2f3149e0634d513065214efb3967f1ab9c46
vkro <vkroeker@gmail.com>
2019-11-26 17:58:08 -0800

    reset name state on Cancel button click

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -8,6 +8,10 @@ export default function Form(props) {
   const [ name, setName ] = useState( props.name || "" );
   const [ interviewer, setInterviewer ] = useState( props.interviewer || null );
 
+  const reset = function() {
+    setName("")
+    setInterviewer(null)
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -29,8 +33,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm onclick={(event) => console.log("buttonclicked")}>Save</Button>
+          <Button danger onClick={event => reset()}>Cancel</Button>
+          <Button confirm onClick={(event) => console.log("buttonclicked")}>Save</Button>
         </section>
       </section>
     </main>
