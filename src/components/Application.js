7940ffb2f8c08fbda248bc8a1cc5477c5a70dac4
vkro <vkroeker@gmail.com>
2019-11-27 15:36:41 -0800

    update all state sets for combined setState obj

--- src/components/Application.js
+++ src/components/Application.js
@@ -52,11 +52,12 @@ export default function Application(props) {
     appointments: {}
   });
 
+  const setDay = day => setState({ ...state, day});
+  const setDays = days => setState(prev => ({ ...prev, days}));
+
   useEffect(() => {
     axios.get("/api/days")
-      .then((response) => {
-        setDays(response.data)
-      })
+      .then(response => setDays(response.data))
      }, []);
 
   const appointments = appointments1.map(appointment => {
@@ -76,8 +77,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state['days']}
+            day={state['day']}
             setDay={setDay}
           />
         </nav>
