fa6ab0490eb1b0e448e5a03a7a6dc53c45d53601
vkro <vkroeker@gmail.com>
2019-11-27 15:05:51 -0800

    get appointment details for given day

--- src/helpers/selectors.jsx
+++ src/helpers/selectors.jsx
@@ -2,6 +2,26 @@
 
 export function getAppointmentsForDay(state, day) {
 
+  const stateAppts = state.appointments;
+  
+  // get list of appointments
+  const getAppointments = function(state, day) {
+    let appointments = [];
+    state.days.forEach(obj => {
+      if (obj.name === day) {
+        appointments =  obj.appointments;
+      }
+    });
+    return appointments;
+  };
+  
+  // map appointment details for each appointment id on the list
+  const getAppointmentDetails = function(stateAppts, arr) {
+    return arr.map(x => stateAppts[x]);
+  };
 
+  let appts = getAppointments(state, day);
+  let getobject = getAppointmentDetails(stateAppts, appts);
 
+  return getobject
 };
