fef11808e4d0de91345c455a581eab78f99a1d2b
vkro <vkroeker@gmail.com>
2019-11-28 13:08:55 -0800

    useVisualMode to conditionally render first components and add empty -> create transition

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,30 @@ import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../../hooks/useVisualMode.js";
 
 
 export default function Appointment(props) {
-  
-  const showInterview = (
-    props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty onAdd={props.onAdd}/>
-  )
-  
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (<article className="appointment">
     <Header
-    time={props.time}
+      time={props.time}
     />
-    {showInterview}
+    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+    {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    )}
   </article>)
 
 };
