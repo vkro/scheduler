b1cc170f37649bed3e9b635b45400f5f9b102de9
vkro <vkroeker@gmail.com>
2019-11-27 20:40:38 -0800

    test and inital code added for back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 
 
 export default function useVisualMode(input) {
-
+  // input is only there to set the initial value
   const [mode, setMode] = useState(input);
   
   // transition allows us to advance to any other mode
   function transition(newMode) {
     setMode(newMode)
-  }
+  };
+
+  function back(oldMode) {
+    setMode(oldMode)
+  };
   
-  return { mode, transition }
+  return { mode, transition, back }
 };
