0933534400ca0b58fbc7eb5c605d51a211264bb2
vkro <vkroeker@gmail.com>
2019-11-28 13:22:56 -0800

    add conditional logic for CREATE mode to show Form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "../../hooks/useVisualMode.js";
 
 
@@ -27,6 +28,8 @@ export default function Appointment(props) {
         interviewer={props.interview.interviewer}
       />
     )}
+    {mode === CREATE && <Form
+    interviewers={[]} />}
   </article>)
 
 };
