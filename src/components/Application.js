03b77fd01afdb919cf9fe4fba8b11e0474e793a6
vkro <vkroeker@gmail.com>
2019-11-27 16:43:49 -0800

    fetch interviewers and add to state

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,7 +10,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -18,11 +19,14 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get("http://localhost:8001/api/days")),
-      Promise.resolve(axios.get("http://localhost:8001/api/appointments"))
+      Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
+      Promise.resolve(axios.get("http://localhost:8001/api/interviewers"))
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0]["data"], appointments: all[1]["data"]}))
+      setState(prev => ({...prev, days: all[0]["data"], appointments: all[1]["data"], interviewers: all[2]["data"]}))
     });
   }, [])
+
+  console.log(state.interviewers);
 
   const appointmentArray = getAppointmentsForDay(state, state.day)
 
