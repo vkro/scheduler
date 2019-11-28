980f2a4df750b8d3328a0e28f97eae67f9680943
vkro <vkroeker@gmail.com>
2019-11-27 20:28:21 -0800

    second test and transition function to advance to another mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,7 +3,12 @@ import React, { useState, useEffect } from "react";
 
 export default function useVisualMode(input) {
 
-  const [ mode, setMode ] = useState(input);
-
-  return { mode }
+  const [mode, setMode] = useState(input);
+  
+  // transition allows us to advance to any other mode
+  function transition(newMode) {
+    setMode(newMode)
+  }
+  
+  return { mode, transition }
 };
