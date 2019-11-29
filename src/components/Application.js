7547b94a2b618864622baf1996eed094e56d731f
vkro <vkroeker@gmail.com>
2019-11-28 21:15:26 -0800

    put request updates database (unresolved error)

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,8 @@ import DayList from "components/DayList";
 import Appointment from "components/Appointment/index"
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors.jsx"
 import "components/Application.scss";
+import useVisualMode from "hooks/useVisualMode.js";
+
 
 export default function Application(props) {
 
@@ -22,16 +24,20 @@ export default function Application(props) {
       Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
       Promise.resolve(axios.get("http://localhost:8001/api/interviewers"))
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0]["data"], appointments: all[1]["data"], interviewers: all[2]["data"]}))
+      setState(prev => ({ ...prev, days: all[0]["data"], appointments: all[1]["data"], interviewers: all[2]["data"] }))
     });
   }, [])
 
-  console.log(state.interviewers);
+  const SHOW = "SHOW";
+  const EMPTY = "EMPTY";
 
   const appointments = getAppointmentsForDay(state, state.day)
   const interviewers = getInterviewersForDay(state, state.day)
+  const { mode, transition, back } = useVisualMode(
+    props.appointments ? SHOW : EMPTY
+  );
 
-  const bookInterview = function(id, interview) {
+  const bookInterview = function (id, interview) {
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -42,22 +48,29 @@ export default function Application(props) {
       [id]: appointment
     };
 
-    setState({
-      ...state, appointments
-    });
+    axios({
+      method: 'put',
+      url: `http://localhost:8001/api/appointments/${id}`,
+      data: { interview: { ...interview } }
+    })
+      .then((response) => {
+        console.log(response.body)
+      })
+      .resolve(transition(SHOW))
+      .catch(err => `this is the error ${err}`)
   };
 
   const schedule = appointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
     return (
       <Appointment
-        key={appointment.id} 
+        key={appointment.id}
         id={appointment.id}
         time={appointment.time}
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
-        />
+      />
     )
   });
   // return of top level Application function
