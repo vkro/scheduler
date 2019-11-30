613c645fbe744d0f933ecf4f16df86ed05c1cb0b
vkro <vkroeker@gmail.com>
2019-11-29 17:48:50 -0800

    function name

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 
-export default function useVisualMode(initial) {
+export default function useApplicationData() {
 
   const [state, setState] = useState({
     day: "Monday",
@@ -21,7 +21,13 @@ export default function useVisualMode(initial) {
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0]["data"], appointments: all[1]["data"], interviewers: all[2]["data"] }))
     });
-  }, [])
+  }, []);
+
+
+
+
+
+
 
   return {
     state,
