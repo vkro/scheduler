47e9575363e00ebaa82e940a6ed40e97009bf1ae
vkro <vkroeker@gmail.com>
2019-11-29 17:45:04 -0800

    state, setDay, and get requests moved to useApplicationData

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,29 +4,16 @@ import DayList from "components/DayList";
 import Appointment from "components/Appointment/index"
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors.jsx"
 import "components/Application.scss";
-import useVisualMode from "hooks/useVisualMode.js";
+import useApplicationData from "hooks/useApplicationData";
 
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => setState({ ...state, day });
-
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(axios.get("http://localhost:8001/api/days")),
-      Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
-      Promise.resolve(axios.get("http://localhost:8001/api/interviewers"))
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0]["data"], appointments: all[1]["data"], interviewers: all[2]["data"] }))
-    });
-  }, [])
+  const {
+    state,
+    setDay,
+    setState
+  } = useApplicationData();
 
   const appointments = getAppointmentsForDay(state, state.day)
   const interviewers = getInterviewersForDay(state, state.day)
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,15 +1,33 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
 export default function useVisualMode(initial) {
 
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
 
+  const setDay = day => setState({ ...state, day });
 
-
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(axios.get("http://localhost:8001/api/days")),
+      Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
+      Promise.resolve(axios.get("http://localhost:8001/api/interviewers"))
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0]["data"], appointments: all[1]["data"], interviewers: all[2]["data"] }))
+    });
+  }, [])
 
   return {
     state,
     setDay,
-    bookInterview,
-    cancelInterview
+    setState
+    //bookInterview,
+    //cancelInterview
   }
 };
