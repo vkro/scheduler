bc3e51ea164a7acf314be5bcae0b72fcd4ff7df5
vkro <vkroeker@gmail.com>
2019-12-03 14:44:24 -0800

    test load data, book interview up to saving mode

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,27 +1,53 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent, getByAltText, getAllByTestId, getByPlaceholderText, getByText, prettyDOM, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 import { exportAllDeclaration } from "@babel/types";
 
-describe('Test', () => {
+describe('Application', () => {
   // >>> Needed this before, but not anymore - nothing changed so keep it just in case???
-  // beforeEach(() => {
-  //   jest.mock('axios', () => require('../../__mocks__/axios.js'))
-  // });
+  beforeEach(() => {
+    jest.mock('axios', () => require('../../__mocks__/axios.js'))
+  });
 
   afterEach(cleanup);
 
   it("changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
-  
+
     await waitForElement(() => getByText("Monday"));
-  
+
     fireEvent.click(getByText("Tuesday"));
-  
+
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"))
+
+    fireEvent.change(getByPlaceholderText(appointment, "Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"))
+    
+    fireEvent.click(getByText(appointment, "Save"))
+
+    // console.log(prettyDOM(appointment));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+
+
+  })
 
 
 
