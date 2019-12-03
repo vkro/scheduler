0d57e0d005d59658f457fbe5fc3d6fa82e92d36b
vkro <vkroeker@gmail.com>
2019-12-03 12:40:54 -0800

    promise chain fires click on Tuesday and verifies document contents

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,21 +1,26 @@
 import React from "react";
 
-import { render, cleanup, waitForElement } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
+import { exportAllDeclaration } from "@babel/types";
 
 describe('Test', () => {
 
-  beforeEach(() => {
-    jest.mock('axios', () => require('../../__mocks__/axios.js'))
-  });
+  // beforeEach(() => {
+  //   jest.mock('axios', () => require('../../__mocks__/axios.js'))
+  // });
 
   afterEach(cleanup);
 
   it("defaults to Monday and changes the schedule when a new day is selected", () => {
     const { getByText } = render(<Application />);
 
-    return waitForElement(() => getByText("Monday"));
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+    
   });
 
 })
