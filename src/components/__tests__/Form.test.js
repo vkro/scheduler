d87c6b9e42b22599800ee936e9814d6a4471c93a
vkro <vkroeker@gmail.com>
2019-12-02 16:17:50 -0800

    boilerplate for form tests

---
+++ src/components/__tests__/Form.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, cleanup, getByPlaceholderText, getByTestId } from "@testing-library/react";
+import Form from "components/Appointment/Form";
+import { isTSAnyKeyword, exportAllDeclaration } from "@babel/types";
+
+afterEach(cleanup);
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  ];
+
+  it("renders without student name if not provided", () => {
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  });
+
+  it("renders with initial student name", () => {
+    expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+});
